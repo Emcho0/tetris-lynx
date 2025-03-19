95be99d5759407f7253e74d250cb814ba9f1e824
Emcho0 <emir.kusundzija@gmail.com>
2025-03-19 13:13:24 +0100

    Dodane tipke lijevo desno i postavi

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { useCallback, useState } from "@lynx-js/react";
 import { Grid } from "./components/Grid.jsx";
 import { ScoreBoard } from "./components/ScoreBoard.jsx";
 import "./styles/App.css";
+import { Controls } from "./components/Controls.jsx";
 
 export function App() {
   // Default theme is Yorumi Shade ("shade")
   const [theme, setTheme] = useState("shade");
-
   const toggleTheme = useCallback(() => {
     setTheme((prev) => (prev === "shade" ? "mist" : "shade"));
   }, []);
@@ -16,15 +16,20 @@ export function App() {
 
   return (
     <view className={appClass}>
+      {/* Tap to toggle theme */}
+      <view className="Grid" bindtap={toggleTheme}>
+        <text className={buttonClass}>Switch Theme</text>
+      </view>
       <view className="Content">
         <text className="Description">Tetris Lynx</text>
         <Grid rows={15} cols={10} />
-        <view className="Score">
-          <ScoreBoard score={0} />
-        </view>
-        {/* Tap to toggle theme */}
-        <view bindtap={toggleTheme}>
-          <text className={buttonClass}>Switch Theme</text>
+        <view className="ScoreAndControls">
+          <view className="Score">
+            <ScoreBoard score={0} />
+          </view>
+          <view className="Controls">
+            <Controls />
+          </view>
         </view>
       </view>
     </view>
---
+++ src/components/Controls.tsx
@@ -0,0 +1,9 @@
+export function Controls() {
+  return (
+    <view className="Controls">
+      <text className="Button">Lijevo</text>
+      <text className="Button">Postavi</text>
+      <text className="Button">Desno</text>
+    </view>
+  );
+}
